ec88c8384af660d00a0732a497fe8ade0a4d900f
Eduard Castro <eduard.cas07@gmail.com>
2019-07-07 21:42:06 -0500

    Add feature login basic

--- src/App.js
+++ src/App.js
@@ -4,17 +4,35 @@ import Home from "./components/Home";
 import Profile from "./components/Profile";
 import Cart from "./components/Cart/Cart";
 import ProductList from "./components/Product/ProductList";
-import Nav from "./components/Nav";
+import Header from "./components/Header";
+import Container from "react-bootstrap/Container";
+import Auth from "./components/Auth/Auth";
+import Callback from "./components/Auth/Callback";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.auth = new Auth(this.props.history);
+  }
+
   render() {
     return (
       <>
-        <Nav />
-        <Route path="/" exact component={Home} />
-        <Route path="/profile" component={Profile} />
-        <Route path="/products" component={ProductList} />
-        <Route path="/cart" component={Cart} />
+        <Header />
+        <Container fluid="true">
+          <Route
+            path="/"
+            exact
+            render={props => <Home auth={this.auth} {...props} />}
+          />
+          <Route
+            path="/callback"
+            render={props => <Callback auth={this.auth} {...props} />}
+          />
+          <Route path="/profile" component={Profile} />
+          <Route path="/products" component={ProductList} />
+          <Route path="/cart" component={Cart} />
+        </Container>
       </>
     );
   }
